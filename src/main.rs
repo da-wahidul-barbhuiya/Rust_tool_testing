2053f401c6b7629b96547aa4cc7c21a3f2cd0f9f
wahid122 <wahidul.b@decodeage.com>
2023-10-27 13:45:09 +0530

    Struct is added to modify the time taking process

--- src/main.rs
+++ src/main.rs
@@ -8,65 +8,55 @@ use std::cmp::Ordering;
 fn after(start:PrimitiveDateTime,time:&u64) ->PrimitiveDateTime{
     start+Duration::new(time*3600, 0)
 }
-fn time_iter(&mut input_vec:Vec<String>,start:Option<PrimitiveDateTime>, end:PrimitiveDateTime){
-        // let mut datetimecout:Vec<String>=Vec::new();
-        if let Some(smallest_time)=start{
-            //println!("Smallest time is from the function :{}",smallest_time);
-            if end > smallest_time{
-                //println!("Parsed time from the function :{:?}",end);
-                input_vec.push(end.to_string());
-                println!("Datetime vector:{:?}",input_vec);
-            }
+// fn time_iter(  &input_vec:Vec<String>,start:Option<PrimitiveDateTime>, end:PrimitiveDateTime){
+//         // let mut datetimecout:Vec<String>=Vec::new();
+//         if let Some(smallest_time)=start{
+//             //println!("Smallest time is from the function :{}",smallest_time);
+//             if end > smallest_time{
+//                 //println!("Parsed time from the function :{:?}",end);
+//                 input_vec.push(end.to_string());
+//                 println!("Datetime vector:{:?}",input_vec);
+//             }
             
-        }
-        else {
-            println!("Not match for this case")
-        }
-    }
-
-// fn get_all_timestamp(file:BufReader<File>,start:PrimitiveDateTime,current:PrimitiveDateTime,end:PrimitiveDateTime){
-//     let adding_time:Vec<String>=Vec::new();
-//     for line in file.lines(){
-//         if start<current && current< end{
-
 //         }
-        
+//         else {
+//             println!("Not match for this case")
+//         }
 //     }
+
+
+
+// #[derive(Debug, Clone, Copy)]
+// struct AddTime<T>{
+//     value: T
 // }
-// fn merge_sort(left:PrimitiveDateTime,right:PrimitiveDateTime)->(Vec<bool>,usize){
-    
 
+// impl<T:Copy+std::ops::AddAssign,R:Into<AddTime<T>>> std::ops::AddAssign<R>
+// for  AddTime<T>
+// {
+//    fn add_assign(&mut self, rhs: R) {
+//        let rhs=rhs.into();
+//        self.value+=rhs.value
+//    } 
 // }
-// fn valid_indices(timestamp:&[PrimitiveDateTime],earliest:&PrimitiveDateTime,latest:&PrimitiveDateTime) -> (){
-//     let mut to_keep:Vec<bool>=vec![false;timestamp.len()];
-//     let mut nb_reads_to_keep=0;
-//     timestamp.iter().enumerate().for_each(|(i,t)|{
-//         if earliest<=t && t<=latest{
-//             to_keep[i]=true;
-//             nb_reads_to_keep+=1;
-//         }
-//         let (Vec,usize)=(&to_keep,nb_reads_to_keep);
-//         ()
-    
+struct AddNothing{
+    input:PrimitiveDateTime,
+    current:PrimitiveDateTime,
+    output:PrimitiveDateTime,
+}
+#[derive(Debug)]
+struct AddTime{
+    smallest_time:String,// for testing use String type only but for actual use PrimitiveDateTime
+    added_time:String,
+    parsed_time:String,
 
-//     });
-// }
-//test from the github page
-
-
-#[derive(Debug, Clone, Copy)]
-struct AddTime<T>{
-    value: T
+}
+#[derive(Debug)]
+struct CheckTime{
+    parsingtime:String
 }
 
-impl<T:Copy+std::ops::AddAssign,R:Into<AddTime<T>>> std::ops::AddAssign<R>
-for  AddTime<T>
-{
-   fn add_assign(&mut self, rhs: R) {
-       let rhs=rhs.into();
-       self.value+=rhs.value
-   } 
-}
+
 fn main() -> std::io::Result<()> {
     let file = File::open("new.fastq")?;
     let reader = BufReader::new(file);
@@ -97,22 +87,36 @@ fn main() -> std::io::Result<()> {
                             println!("QC line: {:?}", fourth_line?);
                         }
                         // smallest+=parsed_datetime
+                        println!("smallest time inside the loop:{:?}",smallest_datetime);
+                        let poke_time=after(smallest, &3);
+                        println!("Poke time inside the loop:{}",poke_time);
+                        let point=AddTime{smallest_time:smallest.to_string(),added_time:poke_time.to_string(),parsed_time:parsed_datetime.to_string()};
+                        println!("Add struct inside the while loop:{:?}",point);
                         
                     }
+                    println!("Parsed date time loop :{}",parsed_datetime.to_string());
+                    let checkme=CheckTime{parsingtime:parsed_datetime.to_string()};
+                    println!("To check time frame:{:?}",checkme);
+                    date_time_take.push(parsed_datetime.to_string());
+                    // println!("Date time take:{:?}",date_time_take);
+
                 } else {
                     smallest_datetime = Some(parsed_datetime);
                 }
-                time_iter(date_time_take,smallest_datetime,parsed_datetime);
+                // time_iter(&date_time_take,smallest_datetime,parsed_datetime);
                 // valid_indices(smallest_datetime, &parsed_datetime, &parsed_datetime)
             }
         }
         
     }
+    println!("Date time taking vector:{:?}",date_time_take);
 
     if let Some(smallest) = smallest_datetime {
         let added_time=after(smallest,&3);
         println!("Smallest datetime frame: {}", smallest);
         println!("New added time is :{}",added_time);
+        // let point=AddTime{smallest_time:smallest.to_string(),added_time:added_time.to_string()};
+        // println!("Struct field value :{:?}",point);
         
     } else {
         println!("No datetime frames found in the file.");
